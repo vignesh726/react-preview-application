12ea28220e81edf6ef005dff6c7bbc96b408db7e
vignesh <vaivignesh726@gmail.com>
2025-03-15 10:29:58 +0530

    update url

--- src/authService.js
+++ src/authService.js
@@ -3,7 +3,7 @@ export const msalConfig = {
     auth: {
       clientId: "55cdc898-c249-45ae-afe5-fbdb4997d321", // Replace with your app's client ID
       authority: "https://login.microsoftonline.com/949d4ab5-4410-40c6-858e-7b44ee2a2257", // Replace with your tenant ID
-      redirectUri: "https://vignesh726.github.io/react-preview-application", // The redirect URI you set during registration
+      redirectUri: "https://vignesh726.github.io/react-preview-application/", // The redirect URI you set during registration
     },
   };  
   export const loginRequest = {
